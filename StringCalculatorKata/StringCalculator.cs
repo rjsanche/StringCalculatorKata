a502f3b5b03169a0e86fcc811e5deccc0ae8bc9d
Raúl Jiménez <rjsanche@gmail.com>
2021-09-01 23:21:45 +0200

    Refactor

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -4,8 +4,10 @@ namespace StringCalculatorKata
 {
     public static class StringCalculator
     {
-        public static int Add(string imput)
+        public static int Add(string input)
         {
+            if(string.IsNullOrEmpty(input))
+                return 0;
             return 0;
         }
     }

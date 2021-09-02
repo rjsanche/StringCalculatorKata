8c32c96b4c32eee8632c52ffccc25c2efa2022c7
Raúl Jiménez <rjsanche@gmail.com>
2021-09-02 23:11:52 +0200

    Refactor

--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -11,16 +11,7 @@ namespace StringCalculatorKata
             {
                 return 0;
             }
-            char[] newDelimiters = null;
-            if (input.StartsWith("//"))
-            {
-                string startNewDelimiterToken = "//";
-                char delimiter = input.Substring(input.IndexOf(startNewDelimiterToken) + startNewDelimiterToken.Length, 1)[0];
-                input = input.Substring(input.IndexOf('\n') + 1);
-                newDelimiters = new char[defaultDelimiters.Length + 1];
-                defaultDelimiters.CopyTo(newDelimiters, 0);
-                newDelimiters[newDelimiters.Length - 1] = delimiter;
-            }
+            char[] newDelimiters = GetNewDelimitersCollection(ref input);
             char[] separator = newDelimiters == null ? defaultDelimiters : newDelimiters;
             string[] slices = input.Split(separator);
             int sum = 0;
@@ -30,6 +21,21 @@ namespace StringCalculatorKata
             }
             return sum;
 
+        }
+
+        private static char[] GetNewDelimitersCollection(ref string input)
+        {
+            if (input.StartsWith("//"))
+            {
+                string startNewDelimiterToken = "//";
+                char delimiter = input.Substring(input.IndexOf(startNewDelimiterToken) + startNewDelimiterToken.Length, 1)[0];
+                input = input.Substring(input.IndexOf('\n') + 1);
+                char[] newDelimiters = new char[defaultDelimiters.Length + 1];
+                defaultDelimiters.CopyTo(newDelimiters, 0);
+                newDelimiters[newDelimiters.Length - 1] = delimiter;
+                return newDelimiters;
+            }
+            return null;
         }
 
 
